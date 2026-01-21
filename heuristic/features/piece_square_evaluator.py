29b5aaee42da0d6f2686e35985152b4be965e448
EmanueleBoa <emanuele.boattini@gmail.com>
2026-01-21 18:22:13 +0100

    refactor tables

--- heuristic/features/piece_square_evaluator.py
+++ heuristic/features/piece_square_evaluator.py
@@ -4,78 +4,71 @@ import chess
 
 from heuristic.features.feature_evaluator import FeatureEvaluator
 
-PST_TABLES = {
-    "pawn_square_table": list(reversed([
-        0, 0, 0, 0, 0, 0, 0, 0,
-        50, 50, 50, 50, 50, 50, 50, 50,
-        10, 10, 20, 30, 30, 20, 10, 10,
-        5, 5, 10, 25, 25, 10, 5, 5,
-        0, 0, 0, 20, 20, 0, 0, 0,
-        5, -5, -10, 0, 0, -10, -5, 5,
-        5, 10, 10, -20, -20, 10, 10, 5,
-        0, 0, 0, 0, 0, 0, 0, 0
-    ])),
-    "knight_square_table": list(reversed([
-        -50, -40, -30, -30, -30, -30, -40, -50,
-        -40, -20, 0, 0, 0, 0, -20, -40,
-        -30, 0, 10, 15, 15, 10, 0, -30,
-        -30, 5, 15, 20, 20, 15, 5, -30,
-        -30, 0, 15, 20, 20, 15, 0, -30,
-        -30, 5, 10, 15, 15, 10, 5, -30,
-        -40, -20, 0, 5, 5, 0, -20, -40,
-        -50, -40, -30, -30, -30, -30, -40, -50
-    ])),
-    "bishop_square_table": list(reversed([
-        -20, -10, -10, -10, -10, -10, -10, -20,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -10, 0, 5, 10, 10, 5, 0, -10,
-        -10, 5, 5, 10, 10, 5, 5, -10,
-        -10, 0, 10, 10, 10, 10, 0, -10,
-        -10, 10, 10, 10, 10, 10, 10, -10,
-        -10, 5, 0, 0, 0, 0, 5, -10,
-        -20, -10, -10, -10, -10, -10, -10, -20
-    ])),
-    "rook_square_table": list(reversed([
-        0, 0, 0, 0, 0, 0, 0, 0,
-        5, 10, 10, 10, 10, 10, 10, 5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        -5, 0, 0, 0, 0, 0, 0, -5,
-        0, 0, 0, 5, 5, 0, 0, 0
-    ])),
-    "queen_square_table": list(reversed([
-        -20, -10, -10, -5, -5, -10, -10, -20,
-        -10, 0, 0, 0, 0, 0, 0, -10,
-        -10, 0, 5, 5, 5, 5, 0, -10,
-        -5, 0, 5, 5, 5, 5, 0, -5,
-        0, 0, 5, 5, 5, 5, 0, -5,
-        -10, 5, 5, 5, 5, 5, 0, -10,
-        -10, 0, 5, 0, 0, 0, 0, -10,
-        -20, -10, -10, -5, -5, -10, -10, -20
-    ])),
-    "king_square_table": list(reversed([
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -30, -40, -40, -50, -50, -40, -40, -30,
-        -20, -30, -30, -40, -40, -30, -30, -20,
-        -10, -20, -20, -20, -20, -20, -20, -10,
-        20, 20, 0, 0, 0, 0, 20, 20,
-        20, 30, 10, 0, 0, 10, 30, 20
-    ])),
-    "king_endgame_square_table": list(reversed([
-        -50, -40, -30, -20, -20, -30, -40, -50,
-        -30, -20, -10, 0, 0, -10, -20, -30,
-        -30, -10, 20, 30, 30, 20, -10, -30,
-        -30, -10, 30, 40, 40, 30, -10, -30,
-        -30, -10, 30, 40, 40, 30, -10, -30,
-        -30, -10, 20, 30, 30, 20, -10, -30,
-        -30, -30, 0, 0, 0, 0, -30, -30,
-        -50, -30, -30, -30, -30, -30, -30, -50
-    ]))
-}
+PAWN_TABLE = list(reversed([
+    0, 0, 0, 0, 0, 0, 0, 0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5, 5, 10, 25, 25, 10, 5, 5,
+    0, 0, 0, 20, 20, 0, 0, 0,
+    5, -5, -10, 0, 0, -10, -5, 5,
+    5, 10, 10, -20, -20, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0
+]))
+
+KNIGHT_TABLE = list(reversed([
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+]))
+
+BISHOP_TABLE = list(reversed([
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+]))
+
+ROOK_TABLE = list(reversed([
+    0, 0, 0, 0, 0, 0, 0, 0,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    0, 0, 0, 5, 5, 0, 0, 0
+]))
+
+QUEEN_TABLE = list(reversed([
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    -10, 0, 5, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20
+]))
+
+KING_TABLE = list(reversed([
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    20, 30, 10, 0, 0, 10, 30, 20
+]))
 
 KING_ENDGAME_TABLE = list(reversed([
     -50, -40, -30, -20, -20, -30, -40, -50,
@@ -92,16 +85,14 @@ KING_ENDGAME_TABLE = list(reversed([
 class PieceSquareEvaluator(FeatureEvaluator):
     def __init__(self, params: Dict[str, List[float]]):
         self.pst_tables: Dict[chess.PieceType, List[float]] = {
-            chess.PAWN: params.get("pawn_square_table", PST_TABLES["pawn_square_table"]),
-            chess.KNIGHT: params.get("knight_square_table", PST_TABLES["knight_square_table"]),
-            chess.BISHOP: params.get("bishop_square_table", PST_TABLES["bishop_square_table"]),
-            chess.ROOK: params.get("rook_square_table", PST_TABLES["rook_square_table"]),
-            chess.QUEEN: params.get("queen_square_table", PST_TABLES["queen_square_table"]),
-            chess.KING: params.get("king_square_table", PST_TABLES["king_square_table"])
+            chess.PAWN: params.get("pawn_square_table", PAWN_TABLE),
+            chess.KNIGHT: params.get("knight_square_table", KNIGHT_TABLE),
+            chess.BISHOP: params.get("bishop_square_table", BISHOP_TABLE),
+            chess.ROOK: params.get("rook_square_table", ROOK_TABLE),
+            chess.QUEEN: params.get("queen_square_table", QUEEN_TABLE),
+            chess.KING: params.get("king_square_table", KING_TABLE)
         }
-        self.king_endgame_table: List[float] = params.get(
-            "king_endgame_square_table", PST_TABLES["king_endgame_square_table"]
-        )
+        self.king_endgame_table: List[float] = params.get("king_endgame_square_table", KING_ENDGAME_TABLE)
 
     def evaluate(self, board: chess.Board, color: bool, *, phase_value: float = 1.0) -> float:
         score = 0.0
