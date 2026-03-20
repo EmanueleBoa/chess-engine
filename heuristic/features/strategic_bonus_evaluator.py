01c7ad83e001ad264c32164b2ed964372d470568
EmanueleBoa <emanuele.boattini@gmail.com>
2026-03-20 08:19:31 +0100

    add queen-piece coordination

--- heuristic/features/strategic_bonus_evaluator.py
+++ heuristic/features/strategic_bonus_evaluator.py
@@ -3,6 +3,7 @@ from typing import Dict
 import chess
 
 from heuristic.features.feature_evaluator import FeatureEvaluator
+from heuristic.features.utils import is_on_same_line, is_on_same_diagonal
 
 BISHOP_PAIR = 40
 ROOK_OPEN = 20
@@ -13,6 +14,9 @@ TRAPPED_PIECE = 50.0
 ROOK_BATTERY = 25.0
 ROOK_CONNECTION = 25.0
 PIECE_ANCHOR = 15.0
+QUEEN_ROOK_BATTERY = 15.0
+QUEEN_BISHOP_BATTERY = 20.0
+QUEEN_KNIGHT_COORDINATION = 25.0
 
 LIGHT_SQUARES = chess.BB_LIGHT_SQUARES
 DARK_SQUARES = chess.BB_DARK_SQUARES
@@ -29,6 +33,9 @@ class StrategicBonusEvaluator(FeatureEvaluator):
         self.rook_battery = params.get("rook_battery", ROOK_BATTERY)
         self.rook_connection = params.get("rook_connection", ROOK_CONNECTION)
         self.piece_anchor = params.get("piece_anchor", PIECE_ANCHOR)
+        self.queen_rook_battery = params.get("queen_rook_battery", QUEEN_ROOK_BATTERY)
+        self.queen_bishop_battery = params.get("queen_bishop_battery", QUEEN_BISHOP_BATTERY)
+        self.queen_knight_coordination = params.get("queen_knight_coordination", QUEEN_KNIGHT_COORDINATION)
 
         self.seventh_rank_mask = {
             chess.WHITE: chess.BB_RANK_7,
@@ -43,8 +50,11 @@ class StrategicBonusEvaluator(FeatureEvaluator):
         knights = board.knights & own_pieces
         bishops = board.bishops & own_pieces
         rooks = board.rooks & own_pieces
+        queens = board.queens & own_pieces
+        enemy_king_square = board.king(not color)
         enemy_pawns = board.pawns & board.occupied_co[not color]
 
+        # PIECE STABILITY
         pawn_attacks = 0
         for pawn_sq in chess.scan_forward(pawns):
             pawn_attacks |= chess.BB_PAWN_ATTACKS[color][pawn_sq]
@@ -71,9 +81,11 @@ class StrategicBonusEvaluator(FeatureEvaluator):
 
             score += piece_status_score * phase_value
 
+        # BISHOP PAIR
         if bishops.bit_count() >= 2:
             score += self.bishop_pair
 
+        # ROOK 7th RANK & OPEN ROOK
         rank_7_mask = self.seventh_rank_mask[color]
         for square in chess.scan_forward(rooks):
             if (1 << square) & rank_7_mask:
@@ -87,6 +99,7 @@ class StrategicBonusEvaluator(FeatureEvaluator):
                 else:
                     score += self.rook_open / 2.0
 
+        # BAD BISHOP
         if bishops & chess.BB_LIGHT_SQUARES:
             pawn_count = (pawns & chess.BB_LIGHT_SQUARES).bit_count()
             if pawn_count > 2:
@@ -96,6 +109,7 @@ class StrategicBonusEvaluator(FeatureEvaluator):
             if pawn_count > 2:
                 score -= (pawn_count - 2) * self.bad_bishop
 
+        # TRAPPED PIECES
         trapped_knights = knights & chess.BB_CORNERS
         for square in chess.scan_forward(trapped_knights):
             if int(board.attacks(square)).bit_count() < 3:
@@ -112,6 +126,7 @@ class StrategicBonusEvaluator(FeatureEvaluator):
                 if (occupied & (1 << chess.F8)):
                     score -= self.trapped_piece
 
+        # ROOK BATTERY & CONNECTION
         num_rooks = rooks.bit_count()
         if num_rooks >= 2:
             rook_squares = list(chess.scan_forward(rooks))
@@ -127,5 +142,24 @@ class StrategicBonusEvaluator(FeatureEvaluator):
                 between_mask = chess.between(square1, square2)
                 if not (between_mask & board.occupied):
                     score += self.rook_connection * phase_value
+
+        # QUEEN COORDINATION
+        if queens:
+            queen_square = next(chess.scan_forward(queens))
+            queen_attacks = board.attacks(queen_square)
+            enemy_king_zone = chess.BB_KING_ATTACKS[enemy_king_square]
+
+            for bishop_square in chess.scan_forward(bishops):
+                if board.attacks(bishop_square) & queen_attacks:
+                    if is_on_same_diagonal(bishop_square, queen_square):
+                        score += self.queen_bishop_battery * phase_value
+
+            for rook_sq in chess.scan_forward(rooks):
+                if is_on_same_line(queen_square, rook_sq) and (board.attacks(rook_sq) & queen_attacks):
+                    score += self.queen_rook_battery * phase_value
+
+            for night_square in chess.scan_forward(knights):
+                if (board.attacks(night_square) & enemy_king_zone) and (queen_attacks & enemy_king_zone):
+                    score += self.queen_knight_coordination * phase_value
 
         return score
--- heuristic/features/utils.py
+++ heuristic/features/utils.py
@@ -7,3 +7,16 @@ def is_central_file(file: int) -> bool:
 
 def is_king_too_advanced(king_rank: int, color: bool) -> bool:
     return (color == chess.WHITE and king_rank > 1) or (color == chess.BLACK and king_rank < 6)
+
+
+def is_on_same_diagonal(square1: int, square2: int) -> bool:
+    file1, rank1 = chess.square_file(square1), chess.square_rank(square1)
+    file2, rank2 = chess.square_file(square2), chess.square_rank(square2)
+
+    return (rank1 - file1 == rank2 - file2) or (rank1 + file1 == rank2 + file2)
+
+
+def is_on_same_line(square1: int, square2: int) -> bool:
+    is_same_file = chess.square_file(square1) == chess.square_file(square2)
+    is_same_rank = chess.square_rank(square1) == chess.square_rank(square2)
+    return is_same_file or is_same_rank
